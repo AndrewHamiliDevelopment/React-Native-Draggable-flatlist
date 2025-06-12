db04c62e6c553e1d8b1f0ee2212cb01e41a759ea
andrewhamili <andrewhamilidevelopment@gmail.com>
2025-06-12 10:04:14 +0800

    react-native-drax

--- src/navigation/screens/Home.tsx
+++ src/navigation/screens/Home.tsx
@@ -3,6 +3,7 @@ import { StaticScreenProps } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, TouchableOpacity, View } from "react-native";
 import DraggableFlatList from "react-native-draggable-flatlist";
+import { DraxList, DraxListItem, DraxProvider } from "react-native-drax";
 import { FlatList, GestureHandlerRootView } from "react-native-gesture-handler";
 
 interface Product {
@@ -41,25 +42,49 @@ export function Home(props: HomeProps) {
 
   return (
     <GestureHandlerRootView>
-      <DraggableFlatList
-        numColumns={twoColumn ? 2 : undefined}
-        data={products}
-        onDragEnd={({ data }) => setProducts(data)}
-        renderItem={({ item, drag, isActive }) => (
-          <TouchableOpacity
-            onLongPress={drag}
-            style={{
-              margin: 48,
-              borderWidth: isActive ? 2 : undefined,
-              borderColor: isActive ? "red" : undefined,
-            }}
-          >
-            <ProductCard text={`${item.name}`} />
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item) => `${item.id}`}
-      />
+      <DraxProvider>
+        <DraxList
+          data={products}
+          numColumns={twoColumn ? 2 : undefined}
+          renderItem={(info, itemProps) => (
+            <DraxListItem
+              style={{margin: 48}}
+              itemProps={itemProps}
+              draggingStyle={{ opacity: 0.3 }}
+            >
+              <ProductCard text={`${info.item.name}`} />
+            </DraxListItem>
+          )}
+          onItemReorder={({ fromIndex, toIndex }) => {
+            // Update your data here
+            const newData = [...products];
+            const item = newData.splice(fromIndex, 1)[0];
+            newData.splice(toIndex, 0, item);
+            setProducts(newData);
+          }}
+        />
+      </DraxProvider>
     </GestureHandlerRootView>
+    // <GestureHandlerRootView>
+    //   <DraggableFlatList
+    //     numColumns={twoColumn ? 2 : undefined}
+    //     data={products}
+    //     onDragEnd={({ data }) => setProducts(data)}
+    //     renderItem={({ item, drag, isActive }) => (
+    //       <TouchableOpacity
+    //         onLongPress={drag}
+    //         style={{
+    //           margin: 48,
+    //           borderWidth: isActive ? 2 : undefined,
+    //           borderColor: isActive ? "red" : undefined,
+    //         }}
+    //       >
+    //         <ProductCard text={`${item.name}`} />
+    //       </TouchableOpacity>
+    //     )}
+    //     keyExtractor={(item) => `${item.id}`}
+    //   />
+    // </GestureHandlerRootView>
   );
 }
 
